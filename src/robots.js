e362e0e20ec7d0334d0c7f2f7261ef1bbc6e3342
Biplab <behind.vampire@gmail.com>
2020-08-15 00:13:43 +0530

    Fixed Vulnerabilities

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id:1,
+        name : 'Somaya Sundar',
+        username : 'Somaya',
+        email : 'somaya.sundar@horrible.com',
+    },
+    {
+        id:2,
+        name : 'Duliya Dom',
+        username : 'Duliya',
+        email : 'Duliya.Dom@horrible.com',
+    },
+    {
+        id:3,
+        name : 'Datun Dutta',
+        username : 'Datun',
+        email : 'Datun.Dutta@horrible.com',
+    },
+    {
+        id:4,
+        name : 'Jibon Das',
+        username : 'Jibon',
+        email : 'Jibon.Das@horrible.com',
+    },
+    {
+        id:5,
+        name : 'Bissadhar Bighat',
+        username : 'Bissadhar',
+        email : 'Bisu.Big@horrible.com',
+    },
+    {
+        id:6,
+        name : 'Charui Chedor',
+        username : 'Charui',
+        email : 'Charui.Chedor@horrible.com',
+    },
+    {
+        id:7,
+        name : 'Tatha Taksal',
+        username : 'Tatha',
+        email : 'Tatha.Taksal@horrible.com',
+    },
+    {
+        id:8,
+        name : 'Vishunath Vairagya',
+        username : 'Vishu',
+        email : 'Vishu.vai@horrible.com',
+    },
+];
